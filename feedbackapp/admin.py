3a7c914e3430398e602c41e4951aafd9fd18ea22
mbnahmadi <mbn.ahmadi78@gmail.com>
2025-08-27 18:40:06 +0330

    change list display feedback model

--- feedbackapp/admin.py
+++ feedbackapp/admin.py
@@ -5,18 +5,26 @@ from .models import FeedBackModel, FeedBackResponseModel
 
 @admin.register(FeedBackModel)
 class FeedBackAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'through', 'date']
-    # list_filter = ['project', 'through', 'date']
+    list_display = ['get_company_name', 'get_location', 'through', 'date']
+    # list_filter = ['get_company_name', 'get_location', 'through', 'date']
 
-# def get_company_name(self, obj):
-#     return obj.project.company_name
-# get_company_name.short_description = 'company'
+    def get_company_name(self, obj):
+        return obj.project.company_name
+    get_company_name.short_description = 'company'
 
-# def get_location(self, obj):
-#     return obj.project.location
-# get_location.short_description = 'location'
+    def get_location(self, obj):
+        return obj.project.location
+    get_location.short_description = 'location'
 
 @admin.register(FeedBackResponseModel)
 class FeedBackResponseAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'through', 'date']
-    # list_filter = ['project', 'through', 'date']
+    list_display = ['get_company_name', 'get_location', 'through', 'date']
+    # list_filter = ['get_company_name', 'get_location', 'through', 'date']
+
+    def get_company_name(self, obj):
+        return obj.feedback.project.company_name
+    get_company_name.short_description = 'company'
+
+    def get_location(self, obj):
+        return obj.feedback.project.location
+    get_location.short_description = 'location'
