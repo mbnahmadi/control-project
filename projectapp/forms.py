bd29fede9d212c543a8469b45cefd913f8a0dcad
mbnahmadi <mbn.ahmadi78@gmail.com>
2025-09-03 13:33:24 +0330

    delete widget map form instrad  add clean and save

--- projectapp/forms.py
+++ projectapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.gis.db import models as gis_models
-from django.contrib.gis.forms import PointField, LineStringField
+from django.contrib.gis.geos import Point, LineString
+from django.core.exceptions import ValidationError
 from django.contrib.gis.geos import Point, LineString
 from .models import ProjectModel
 
@@ -44,10 +45,61 @@ class GeometryTextArea(forms.Textarea):
             )
 
 class ProjectAdminForm(forms.ModelForm):
+    geometry_input = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": 3, "cols": 40}),
+        required=False,
+        label='Geometry Input',
+        help_text='Enter as "lat,lon" for Point or "lat1,lon1; lat2,lon2; ..." for LineString'
+    )
 
     class Meta:
         model = ProjectModel
         fields = "__all__"
-        widgets = {
-            'geometry': GeometryTextArea(attrs={"rows": 3, "cols": 40}),
-        }
+        # widgets = {
+        #     'geometry': GeometryTextArea(attrs={"rows": 3, "cols": 40}),
+        # }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.geometry:
+            if self.instance.is_point():
+                p = self.instance.geometry
+                self.fields['geometry_input'].initial = f"{p.y},{p.x}"
+            elif self.instance.is_line():
+                coords = list(self.instance.geometry.coords)
+                self.fields['geometry_input'].initial = "; ".join(
+                f"{y},{x}" for (x, y) in coords
+            )
+                # self.fields['geometry_input'].initial = "; ".join([f"{p.y},{p.x}" for p in self.instance.geometry])
+
+    def clean(self):
+        cleaned_data = super().clean()
+        raw_input = cleaned_data.get('geometry_input')
+        
+        if raw_input:
+            try:
+                pairs = [p.strip() for p in raw_input.split(";") if p.strip()]
+                points = []
+                for pair in pairs:
+                    lat, lon = pair.split(",")
+                    points.append((float(lon.strip()), float(lat.strip())))
+                if len(points) == 1:
+                    cleaned_data['geometry'] = Point(points[0], srid=4326)
+                    # print('cleaned_data[geometry]', cleaned_data['geometry'])
+                elif len(points) >= 2:
+                    cleaned_data['geometry'] = LineString(points, srid=4326)
+                else:
+                    raise ValueError("At least 1 point required.")
+            except Exception as e:
+                raise ValidationError(f"Invalid geometry input: {str(e)}")
+        return cleaned_data
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)  # اول instance آماده کن بدون save
+        if 'geometry' in self.cleaned_data:  # اگر geometry بروز شده
+            instance.geometry = self.cleaned_data['geometry']
+            # print('instance.geometry', instance.geometry)
+
+        if commit:
+            instance.save()  # حالا save واقعی
+            self.save_m2m()  # اگر m2m فیلدی داری
+        return instance
