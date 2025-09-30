69e66c506383bdbb108751aab8ebd18b059983e2
mbnahmadi <mbn.ahmadi78@gmail.com>
2025-09-30 14:08:24 +0330

    delete cache befor execute task

--- projectapp/tasks.py
+++ projectapp/tasks.py
@@ -1,9 +1,18 @@
 import time
 from celery import shared_task
 from .models import ProjectModel
+from django.core.cache import cache
+
 
 @shared_task
 def update_active_projects_pdfs():
+    qs = ProjectModel.active_locations.only('id', 'project_address', 'latest_pdf_path').iterator()
+    companies = set()
+    for p in qs:
+        if p.project_address:
+            companies.add(p.project_address.split('/', 1)[0])
+    for company in companies:
+        cache.delete(f'latest_gfs_{company}')
     active_projects = ProjectModel.active_locations.only('id', 'project_address', 'latest_pdf_path').iterator()
     for project in active_projects:
         new_path = project.generate_latest_pdf_address()
